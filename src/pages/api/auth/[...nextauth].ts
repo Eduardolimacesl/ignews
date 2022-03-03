b8a478aa93e5c378b3a70cb5acdbc2e849d56876
@eduardoLima <eduardolimacesl@gmail.com>
2022-03-03 16:22:12 -0300

    Ignite>ReactJS>Back-end no front-end

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,9 +14,14 @@ export default NextAuth({
   // ...add more providers here. exemple: google.
   ],
   callbacks: {
-    async signIn(user, account, profile) {
-      console.log(user);
-      return true;
-    }
+    async signIn({ user, account, profile, email, credentials }) {
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: {email}}
+        )
+      )      
+      return true
+    },
   }
 })
